e6cb761206efd3277c66d9948e6de1b5093f3f3c
Eric Viana <eric@bipa.app>
2024-12-04 09:12:41 -0300

    feat: add tokens

---
+++ Sources/Apolo/Tokens/Tokens.swift
@@ -0,0 +1,38 @@
+//
+//  Tokens.swift
+//  Apolo
+//
+//  Created by Eric on 04/12/24.
+//
+
+import Foundation
+
+enum Tokens {
+    enum Spacing {
+        static let zero: CGFloat = 0
+
+        static let extraExtraSmall: CGFloat = 4
+
+        static let extraSmall: CGFloat = 8
+
+        static let small: CGFloat = 12
+
+        static let medium: CGFloat = 16
+
+        static let large: CGFloat = 20
+
+        static let extraLarge: CGFloat = 32
+
+        static let extraExtraLarge: CGFloat = 40
+    }
+
+    enum CornerRadius {
+        static let zero: CGFloat = 0
+
+        static let small: CGFloat = 8
+
+        static let medium: CGFloat = 12
+
+        static let large: CGFloat = 20
+    }
+}

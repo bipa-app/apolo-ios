043114e944076dc9d43ae4ec1163ee045fb947a3
Eric Viana <eric@bipa.app>
2024-12-05 11:11:50 -0300

    fix: make token variables public and static

--- Sources/Apolo/Tokens/Tokens.swift
+++ Sources/Apolo/Tokens/Tokens.swift
@@ -7,32 +7,32 @@
 
 import Foundation
 
-enum Tokens {
-    enum Spacing {
-        static let zero: CGFloat = 0
+public enum Tokens {
+    public enum Spacing {
+        public static let zero: CGFloat = 0
 
-        static let extraExtraSmall: CGFloat = 4
+        public static let extraExtraSmall: CGFloat = 4
 
-        static let extraSmall: CGFloat = 8
+        public static let extraSmall: CGFloat = 8
 
-        static let small: CGFloat = 12
+        public static let small: CGFloat = 12
 
-        static let medium: CGFloat = 16
+        public static let medium: CGFloat = 16
 
-        static let large: CGFloat = 20
+        public static let large: CGFloat = 20
 
-        static let extraLarge: CGFloat = 32
+        public static let extraLarge: CGFloat = 32
 
-        static let extraExtraLarge: CGFloat = 40
+        public static let extraExtraLarge: CGFloat = 40
     }
 
-    enum CornerRadius {
-        static let zero: CGFloat = 0
+    public enum CornerRadius {
+        public static let zero: CGFloat = 0
 
-        static let small: CGFloat = 8
+        public static let small: CGFloat = 8
 
-        static let medium: CGFloat = 12
+        public static let medium: CGFloat = 12
 
-        static let large: CGFloat = 20
+        public static let large: CGFloat = 20
     }
 }

3989736c49fb71a01a837448956920e0fc5a6d04
Eric Viana <eric@bipa.app>
2024-12-23 10:51:11 -0300

    chore: add docc to token units

--- Sources/Apolo/Tokens/Tokens.swift
+++ Sources/Apolo/Tokens/Tokens.swift
@@ -9,30 +9,43 @@ import Foundation
 
 public enum Tokens {
     public enum Spacing {
+        /// No spacing (0)
         public static let zero: CGFloat = 0
 
+        /// Extra extra small spacing (4)
         public static let extraExtraSmall: CGFloat = 4
 
+        /// Extra small spacing (8)
         public static let extraSmall: CGFloat = 8
 
+        /// Small spacing (12)
         public static let small: CGFloat = 12
 
+        /// Medium spacing (16)
         public static let medium: CGFloat = 16
 
+        /// Large spacing (20)
         public static let large: CGFloat = 20
 
+        /// Extra large spacing (32)
         public static let extraLarge: CGFloat = 32
 
+        /// Extra extra large spacing (40)
         public static let extraExtraLarge: CGFloat = 40
     }
 
     public enum CornerRadius {
+        /// No corner radius (0)
         public static let zero: CGFloat = 0
 
+        /// Small corner radius (8)
         public static let small: CGFloat = 8
 
+        /// Medium corner radius (12)
         public static let medium: CGFloat = 12
 
+        /// Large corner radius (20)
         public static let large: CGFloat = 20
     }
 }
+

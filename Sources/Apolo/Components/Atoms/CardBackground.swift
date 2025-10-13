ba3e5b5c4c6ad6f27a294879e831b511f8b917e8
Lucas Farah <lucas.farah@bipa.app>
2025-10-13 13:28:27 -0300

    iOS 26: card background glass

--- Sources/Apolo/Components/Atoms/CardBackground.swift
+++ Sources/Apolo/Components/Atoms/CardBackground.swift
@@ -29,8 +29,13 @@ public struct CardBackground: View {
     }
     
     public var body: some View {
-        RoundedRectangle(cornerRadius: cornerRadius)
-            .fill(color)
+        if #available(iOS 26.0, *) {
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .glassEffect(.regular.tint(color).interactive(), in: RoundedRectangle(cornerRadius: cornerRadius))
+        } else {
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .fill(color)
+        }
     }
 }
 
@@ -99,8 +104,25 @@ public extension View {
         Label("Label", systemImage: "gear")
             .padding()
             .background(CardBackground())
+        
+        VStack(alignment: .leading, spacing: Tokens.Spacing.extraExtraSmall) {
+            Group {
+                Text("Liberação do Satsback®")
+                    .callout(weight: .bold)
+                    .foregroundStyle(Tokens.Color.label.color)
+                
+                Text("Seu Satsback® ficará disponível para resgate após o fechamento e pagamento da primeira fatura do seu cartão de crédito.")
+                    .subheadline()
+                    .foregroundStyle(Tokens.Color.secondaryLabel.color)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .cardBackground(.secondary)
+        .padding(.horizontal, Tokens.Spacing.medium)
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .cardBackground(color: .secondary)
     .padding()
+    .tint(.primary)
 }
